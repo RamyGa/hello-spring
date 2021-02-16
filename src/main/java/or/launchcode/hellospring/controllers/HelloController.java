af03d0d1e64fb459e628830f412370385f514b67
Ramyga <research@ronolabs.com>
2021-02-15 20:48:49 -0600

    built out HelloController

--- src/main/java/or/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/or/launchcode/hellospring/controllers/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("hello")
 public class HelloController {
 
 
@@ -17,28 +18,28 @@ public class HelloController {
     }
 
     @GetMapping("goodbye")//this method should handle get requests
-    @ResponseBody
+    @ResponseBody // this can be added to the top of the class as a single annotation
     public String goodBye(){
         return "goodbye, Spring!";
     }
 
     //handler that handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
+    @ResponseBody// this can be added to the top of the class as a single annotation
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, "+ name + "!";
     }
 
     //handler that handles request of the form /hello/LaunchCode
     @GetMapping("hello/{name}")//we will take that dynamic variable name (which can be anything)
-    @ResponseBody                //and set that var value to the String name below
+    @ResponseBody // this can be added to the top of the class as a single annotation               //and set that var value to the String name below
     public String helloWithPathParam(@PathVariable String name){
 
         return "Hello "+ name + "!";
     }
 
     @GetMapping("/form")
-    @ResponseBody
+    @ResponseBody// this can be added to the top of the class as a single annotation
     public String helloForm(){
         return "<html>" +
                 "<body>" +
@@ -48,6 +49,52 @@ public class HelloController {
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    @GetMapping("/exercise_form")
+    @ResponseBody
+    public String greetingForm(){
+
+        return "<html>" +
+                "<body>" +
+                "<form action='greeting_response' method='post'>" +
+                "<input type'text' name='name'>" +
+                "<select name='language' id='language'>" + // grabbing language value &
+                                                           //applying it to name=language
+                "    <option value='english'>English</option>" +
+                "    <option value='french'>French</option>" +
+                "    <option value='spanish'>Spanish</option>" +
+                "    <option value='arabic'>Arabic</option>" +
+                "  </select>" +
+                "<input type='submit' value='greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    //if its a form the output of the url will be /hello?name=LaunchCode
+    @PostMapping("/greeting_response")
+    @ResponseBody
+    public String greetingResponse(@RequestParam String name, @RequestParam String language){
+
+         return languageGreeting(name, language);
+    }
+
+
+    public String languageGreeting(String name, String language){
+        if(language.equals("english")){
+            return "Hello "+ name;
+        }
+        else if(language.equals("french")){
+            return "Bonjour "+ name;
+        }
+        else if(language.equals("spanish")){
+            return "Hola "+ name;
+        }
+        else {
+            return "Marhaba "+ name;
+        }
+
     }
 
 
